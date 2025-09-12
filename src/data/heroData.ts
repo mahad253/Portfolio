273a0762eb335ece4636d432f3f45def236df5d5
moussma8 <mahad.moussa-abdillahi@univ-rouen.fr>
2025-09-12 16:31:10 +0200

    Del01

--- src/data/heroData.ts
+++ src/data/heroData.ts
@@ -1,7 +1,7 @@
 export const heroData = {
   name: 'Mahad MOUSSA ABDILLAHI',
   title: 'Alternance Développement Full-Stack',
-  description: `Actuellement en Master 2 Génie Informatique Logiciel (GIL) et intéressé par les technologies web, je suis à la recherche d'une alternance en développement Full-Stack.
+  description: `Actuellement en Master 2 Génie Informatique Logiciel (GIL) et intéressé par les technologies web, je suis à la recherche d'un offre de stage de fin d'études dans le développement full-stack.
 J’interviens sur la conception d’applications web, du développement de l’interface utilisateur (frontend) à la mise en place de l’architecture côté serveur (backend), avec une utilisation régulière de Git pour la gestion des versions.`,
   profilePicture: '/assets/mahad.jpg', // à placer dans /public/assets/
   skills: [
