fcaf56dfe2bcebd81af2e93ab7fd74d7cca482ba
moussma8 <mahad.moussa-abdillahi@univ-rouen.fr>
2026-04-28 19:09:45 +0200

    MODIF

--- src/data/heroData.ts
+++ src/data/heroData.ts
@@ -1,6 +1,6 @@
 export const heroData = {
   name: 'Mahad MOUSSA ABDILLAHI',
-  title: 'Stage en Développement Full-Stack',
+  title: 'Développeur Full-Stack',
   description: `Étudiant en Master 2 Génie Informatique Logiciel à l’Université de Rouen, je participe à des projets full-stack combinant développement back-end, front-end et gestion de données, avec une première expérience dans l’intégration de solutions d’intelligence artificielle. J’accorde une attention particulière à la conception d’applications robustes, maintenables et bien structurées, en m’appuyant sur de bonnes pratiques de développement et un travail collaboratif en environnement agile. À la recherche d’un stage de fin d’études de 5 à 6 mois, je souhaite renforcer mes compétences en développement logiciel et contribuer concrètement à des projets techniques ambitieux.`,
   profilePicture: '/assets/mahad.jpg', // à placer dans /public/assets/
   skills: [
